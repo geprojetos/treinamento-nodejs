524d86030801f8890b9e2fc8994e74d79e043448
Genilson <geprojetosbrasil@gmail.com>
2024-09-21 15:42:37 -0300

    orquestração usecases

---
+++ frontend/training/src/@core/index.ts
@@ -0,0 +1,15 @@
+import {
+  useCreateFood,
+  useDeleteFood,
+  useGetAllFoods,
+  useGetOnlyFood,
+  useNavigateDetailFood,
+} from "./useCases/foods"
+
+export {
+  useCreateFood,
+  useDeleteFood,
+  useGetAllFoods,
+  useGetOnlyFood,
+  useNavigateDetailFood,
+}
---
+++ frontend/training/src/@core/useCases/foods/index.ts
@@ -0,0 +1,24 @@
+import Gateway from "../../infra/Gateway"
+import HttpClientAxiosAdapter from "../../infra/HttpAxiosAdapterClient"
+import UseCreateFood from "./UseCreateFood"
+import UseDeleteFood from "./UseDeleteFood"
+import UseGetAllFoods from "./UseGetAllFoods"
+import UseGetOnlyFood from "./UseGetOnlyFood"
+import UseNavigateDetailFood from "./UseNavigateDetailFood"
+
+const baseUrl = "http://localhost:3001"
+const httpClient = new HttpClientAxiosAdapter(baseUrl)
+const gateway = new Gateway(httpClient)
+const useCreateFood = new UseCreateFood(gateway)
+const useDeleteFood = new UseDeleteFood(gateway)
+const useGetAllFoods = new UseGetAllFoods(gateway)
+const useGetOnlyFood = new UseGetOnlyFood(gateway)
+const useNavigateDetailFood = new UseNavigateDetailFood()
+
+export {
+  useCreateFood,
+  useDeleteFood,
+  useGetAllFoods,
+  useGetOnlyFood,
+  useNavigateDetailFood,
+}
